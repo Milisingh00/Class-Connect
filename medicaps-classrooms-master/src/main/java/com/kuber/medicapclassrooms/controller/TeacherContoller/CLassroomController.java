4d45fa739fb948f38cd9e4a21a9e617facc14aa4
Milisingh00 <113784238+Milisingh00@users.noreply.github.com>
2023-12-18 22:43:38 +0530

    Add files via upload

---
+++ medicaps-classrooms-master/src/main/java/com/kuber/medicapclassrooms/controller/TeacherContoller/CLassroomController.java
@@ -0,0 +1,50 @@
+package com.kuber.medicapclassrooms.controller.TeacherContoller;
+
+import com.kuber.medicapclassrooms.model.Student;
+import com.kuber.medicapclassrooms.model.dtos.CLassCodeDto;
+import com.kuber.medicapclassrooms.services.Serviceimpl;
+import com.kuber.medicapclassrooms.utils.RequestResponseMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.MediaType;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+@WebServlet("/teachers/classroom")
+public class CLassroomController extends HttpServlet {
+    public Serviceimpl service;
+    public RequestResponseMapper mapper ;
+
+    public CLassroomController() {
+        this.service = new Serviceimpl();
+        this.mapper = new RequestResponseMapper();
+    }
+    @Override// return list of all students in a class
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+//        CLassCodeDto classCode = (CLassCodeDto) mapper.getRequestObject(resp,req, CLassCodeDto.class);
+        CLassCodeDto classCode = new CLassCodeDto();
+        classCode.setClassCode(req.getParameter("classCode"));
+        List<Student> list = service.getAllStudentsInClass(classCode);
+        resp.setContentType(MediaType.APPLICATION_JSON);
+        if(list.size()>0){
+            out.print(mapper.setResponseObject(list));
+        }else{
+            out.print(mapper.setResponseObject(new ArrayList<String>()));
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+}
---
+++ medicaps-classrooms-master/src/main/java/com/kuber/medicapclassrooms/controller/TeacherContoller/TeacherScoreBoardController.java
@@ -0,0 +1,46 @@
+package com.kuber.medicapclassrooms.controller.TeacherContoller;
+import com.kuber.medicapclassrooms.model.ScoreResponse;
+import com.kuber.medicapclassrooms.model.dtos.QuizIdDto;
+import com.kuber.medicapclassrooms.services.Serviceimpl;
+import com.kuber.medicapclassrooms.utils.RequestResponseMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.MediaType;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/teacher/classrooms/quizzes/score")
+public class TeacherScoreBoardController extends HttpServlet {
+    public Serviceimpl service;
+    public RequestResponseMapper mapper;
+
+    public TeacherScoreBoardController() {
+        this.service = new Serviceimpl();
+        this.mapper = new RequestResponseMapper();
+    }
+
+    @Override // return total score of the quiz
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+//        QuizIdDto quizId = (QuizIdDto) mapper.getRequestObject(resp,req, QuizIdDto.class);
+        QuizIdDto quizId = new QuizIdDto();
+        quizId.setQuizId(Integer.parseInt(req.getParameter("quizId")));
+        List<ScoreResponse> list = service.getScoresOfStudentAttendedTheQuiz(quizId);
+        resp.setContentType(MediaType.APPLICATION_JSON);
+        out.print(mapper.setResponseObject(list));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+}
